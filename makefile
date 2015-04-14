35ba65277f13de88a50c53820cc5c0ed8affa495
Alex Gerstenberger <alexgerst93@gmail.com>
2015-04-14 15:27:43 -0400

    Added OpenSSL library usage

---
+++ makefile
@@ -0,0 +1,24 @@
+SDIR=src
+IDIR =inc
+ODIR=obj
+CC=gcc
+CFLAGS=-I$(IDIR) -lcrypto
+
+_DEPS = frsa.h
+DEPS = $(patsubst %,$(IDIR)/%,$(_DEPS))
+
+_OBJ = frsa.o 
+OBJ = $(patsubst %,$(ODIR)/%,$(_OBJ))
+
+$(ODIR)/%.o: $(SDIR)/%.c $(DEPS)
+	mkdir -p $(@D)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+frsa: $(OBJ)
+	gcc -o $@ $^ $(CFLAGS)
+
+.PHONY: clean
+
+clean:
+	rm -rf $(ODIR)
+	rm frsa 
---
+++ src/frsa.c
@@ -0,0 +1,28 @@
+#include <frsa.h>
+
+RSA *createRSAWithFilename(char *filename, int public)
+{
+    FILE *fp = fopen(filename, "rb");
+    if (fp == NULL)
+    {
+        printf("Unable to open file %s \n", filename);
+        return NULL;    
+    }
+    RSA *rsa = RSA_new();
+
+    if (public)
+    {
+        PEM_read_RSA_PUBKEY(fp, &rsa, NULL, NULL);
+    }
+    else
+    {
+        PEM_read_RSAPrivateKey(fp, &rsa, NULL, NULL);
+    }
+
+    return rsa;
+}
+
+void main()
+{
+    printf("frsa");
+}
